2ac340040ec9748caef8b9757643c4d82285c5e1
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 11:12:45 +0530

    [Author:Ujjwal] ADD:Grocries.java

--- src/com/bridgelabz/smartWarehouseManagement/Grocries.java
+++ src/com/bridgelabz/smartWarehouseManagement/Grocries.java
@@ -1,4 +1,12 @@
 package com.bridgelabz.smartWarehouseManagement;
 
-public class Grocries {
+public class Grocries extends WareHouseItem {
+    public Grocries(String name) {
+        super(name);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Groceries: " + getName());
+    }
 }
