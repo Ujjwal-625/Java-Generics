d84f000acbf5b1b3a19a3fb7d66eac386666baf8
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 13:54:16 +0530

    [Author:Ujjwal] ADD:AssignmentCourse.java

--- src/com/bridgelabz/coursemanagementsystem/AssignmentCourse.java
+++ src/com/bridgelabz/coursemanagementsystem/AssignmentCourse.java
@@ -1,3 +1,16 @@
 package com.bridgelabz.coursemanagementsystem;
 
+public class AssignmentCourse extends CourseType {
+    private int assignmentsCount;
 
+    public AssignmentCourse(String courseName, String instructor, int assignmentsCount) {
+        super(courseName, instructor);
+        this.assignmentsCount = assignmentsCount;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Assignment Course: " + getCourseName() + " | Instructor: " + getInstructor() +
+                " | Total Assignments: " + assignmentsCount);
+    }
+}
