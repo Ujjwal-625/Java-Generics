e39491a9bc9710171e2877dd6b92933de6715553
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 13:49:48 +0530

    [Author:Ujjwal] ADD:ExamCourse.java

--- src/com/bridgelabz/coursemanagementsystem/ExamCourse.java
+++ src/com/bridgelabz/coursemanagementsystem/ExamCourse.java
@@ -1,4 +1,16 @@
 package com.bridgelabz.coursemanagementsystem;
 
-public class ExamCourse {
+public class ExamCourse extends CourseType {
+    private int examWeightage;
+
+    public ExamCourse(String courseName, String instructor, int examWeightage) {
+        super(courseName, instructor);
+        this.examWeightage = examWeightage;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Exam Course: " + getCourseName() + " | Instructor: " + getInstructor() +
+                " | Exam Weightage: " + examWeightage + "%");
+    }
 }
