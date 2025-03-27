36bd7e2b5eb6309955e68b2237ce246126f7e948
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 14:09:03 +0530

    [Author:Ujjwal] ADD:VegetarianMeal.java

--- src/com/bridgelabz/mealplangenerator/VegetarianMeal.java
+++ src/com/bridgelabz/mealplangenerator/VegetarianMeal.java
@@ -1,4 +1,15 @@
 package com.bridgelabz.mealplangenerator;
 
-public class VegetarianMeal {
+public class VegetarianMeal implements MealPlan {
+    private String mainDish;
+
+    public VegetarianMeal(String mainDish) {
+        this.mainDish = mainDish;
+    }
+
+    @Override
+    public void displayMeal() {
+        System.out.println("Vegetarian Meal: " + mainDish);
+    }
 }
+
