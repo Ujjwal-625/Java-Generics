e5574376e72b3ea0d739413f64970881de53f861
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 10:47:19 +0530

    [Author:Ujjwal] ADD:FlightManager.java

---
+++ src/com/bridgelabz/flightSchedulingSystem/FlightManager.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.flightSchedulingSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightManager <T> {
+
+    private List<Flight<T>> flights = new ArrayList<>();
+
+    public void addFlight(Flight<T> flight) {
+        flights.add(flight);
+    }
+
+    public void displayAllFlights() {
+        for (Flight<T> flight : flights) {
+            flight.displayFlightInfo();
+        }
+    }
+
+}
