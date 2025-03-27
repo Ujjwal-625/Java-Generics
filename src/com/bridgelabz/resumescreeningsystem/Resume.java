18410c9315ecd30e2dad11040aea877a3f115403
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 14:28:33 +0530

    [Author:Ujjwal] ADD:Resume.java

--- src/com/bridgelabz/resumescreeningsystem/Resume.java
+++ src/com/bridgelabz/resumescreeningsystem/Resume.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.resumescreeningsystem;
 
+
+import java.util.List;
+
+
 public class Resume<T extends JobRole> {
     private T candidateDetails;
 
@@ -15,3 +19,14 @@ public class Resume<T extends JobRole> {
         candidateDetails.displayJobDetails();
     }
 }
+
+
+class ResumeScreeningUtils {
+    public static void screenCandidates(List<? extends JobRole> candidates) {
+        System.out.println("Screening Candidates:");
+        for (JobRole candidate : candidates) {
+            candidate.displayJobDetails();
+        }
+    }
+}
+
