6e01831fd8184401de0fd0e83a98beb2ff878933
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 11:14:37 +0530

    [Author:Ujjwal] ADD:WareHouseItem.java

--- src/com/bridgelabz/smartWarehouseManagement/WareHouseItem.java
+++ src/com/bridgelabz/smartWarehouseManagement/WareHouseItem.java
@@ -1,4 +1,25 @@
 package com.bridgelabz.smartWarehouseManagement;
 
-public class WareHouseItem {
+import java.util.List;
+
+abstract public class WareHouseItem {
+    private String name;
+
+    public WareHouseItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void display();
+}
+
+class WarehouseUtils {
+    public static void displayItems(List<? extends WareHouseItem> items) {
+        for (WareHouseItem item : items) {
+            item.display();
+        }
+    }
 }
