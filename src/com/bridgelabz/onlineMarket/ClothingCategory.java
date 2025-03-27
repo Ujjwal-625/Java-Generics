a43db4b88fb43b4c9cb3c2ee968a3f0e8c1435af
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 13:32:09 +0530

    [Author:Ujjwal] ADD:ClotihingCategory.java

--- src/com/bridgelabz/onlineMarket/ClothingCategory.java
+++ src/com/bridgelabz/onlineMarket/ClothingCategory.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.onlineMarket;
 
 public class ClothingCategory {
+    private String type;
+
+    public ClothingCategory(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Clothing Type: " + type;
+    }
 }
