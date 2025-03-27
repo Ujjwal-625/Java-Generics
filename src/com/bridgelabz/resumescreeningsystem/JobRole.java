702ae06844867a0799ed9b3208736446f01ed6b2
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 14:21:36 +0530

    [Author:Ujjwal] ADD:JobRole.java

---
+++ src/com/bridgelabz/resumescreeningsystem/JobRole.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.resumescreeningsystem;
+
+
+public abstract class JobRole {
+    private String candidateName;
+    private int experienceYears;
+
+    public JobRole(String candidateName, int experienceYears) {
+        this.candidateName = candidateName;
+        this.experienceYears = experienceYears;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public abstract void displayJobDetails();
+}
