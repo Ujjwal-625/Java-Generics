a765674d3ea9086194ce09ac801aa3425f8398ce
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 11:12:17 +0530

    [Author:Ujjwal] ADD:Furniture.java

--- src/com/bridgelabz/smartWarehouseManagement/Furniture.java
+++ src/com/bridgelabz/smartWarehouseManagement/Furniture.java
@@ -1,4 +1,12 @@
 package com.bridgelabz.smartWarehouseManagement;
 
-public class Furniture {
+public class Furniture extends WareHouseItem {
+    public Furniture(String name) {
+        super(name);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Furniture: " + getName());
+    }
 }
