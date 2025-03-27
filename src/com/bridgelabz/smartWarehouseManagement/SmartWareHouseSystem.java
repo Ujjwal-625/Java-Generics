b5ef8a8c89e80abe2140914743717935f5f4021f
Ujjwal <Ujjwalrawat248@gmail.com>
2025-03-27 11:13:10 +0530

    [Author:Ujjwal] ADD:SmartWareHouseSystem.java

--- src/com/bridgelabz/smartWarehouseManagement/SmartWareHouseSystem.java
+++ src/com/bridgelabz/smartWarehouseManagement/SmartWareHouseSystem.java
@@ -1,4 +1,30 @@
 package com.bridgelabz.smartWarehouseManagement;
 
 public class SmartWareHouseSystem {
+    public static void main(String[] args) {
+        // Creating storages
+        Storage<Electronics> electronicsStorage = new Storage<>();
+        Storage<Grocries> groceriesStorage = new Storage<>();
+        Storage<Furniture> furnitureStorage = new Storage<>();
+
+        // Adding items
+        electronicsStorage.addItem(new Electronics("Laptop"));
+        electronicsStorage.addItem(new Electronics("Smartphone"));
+
+        groceriesStorage.addItem(new Grocries("Apple"));
+        groceriesStorage.addItem(new Grocries("Milk"));
+
+        furnitureStorage.addItem(new Furniture("Chair"));
+        furnitureStorage.addItem(new Furniture("Table"));
+
+        // Displaying items
+        System.out.println("Electronics Storage:");
+        WarehouseUtils.displayItems(electronicsStorage.getItems());
+
+        System.out.println("\nGroceries Storage:");
+        WarehouseUtils.displayItems(groceriesStorage.getItems());
+
+        System.out.println("\nFurniture Storage:");
+        WarehouseUtils.displayItems(furnitureStorage.getItems());
+    }
 }
